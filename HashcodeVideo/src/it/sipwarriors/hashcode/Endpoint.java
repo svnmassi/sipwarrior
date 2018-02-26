322d2968e5836b8c4ea3386d3bbd6a6b14b95ff1
girotini <girotini@192.168.31.1>
2018-02-27 00:35:41 +0100

    metodi di utilità

--- HashcodeVideo/src/it/sipwarriors/hashcode/Endpoint.java
+++ HashcodeVideo/src/it/sipwarriors/hashcode/Endpoint.java
@@ -8,12 +8,24 @@ public class Endpoint {
   private int dataCenterLatancy;
   private Map<Integer, Integer> cacheServersLatency = new HashMap<>();
 
+  public Map<Integer, Integer> getCacheServersLatency() {
+    return cacheServersLatency;
+  }
+
   public Endpoint(int dataCenterLatancy) {
     this.dataCenterLatancy = dataCenterLatancy;
   }
 
+  public int getDataCenterLatancy() {
+    return dataCenterLatancy;
+  }
+
   public void setServerLatency(Integer id, Integer latency) {
     cacheServersLatency.put(id, latency);
+  }
+
+  public Integer getServerLatency(Integer cacheServerId) {
+    return cacheServersLatency.get(cacheServerId);
   }
 
 }
